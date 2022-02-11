e2fbd1cdd9143e270e1720b064574b94cb45dae0
Souvik Halder <souvik.halder2020@gift.edu.in>
2022-02-11 20:10:03 +0530

    video 45

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -38,4 +38,43 @@ body('description','Enter atleast a 5 character of descirption').isLength({ min:
 }
     })
 
+
+//Router3:-- Update a new Note using : PUT"/api/auth/updatenote". Login required
+
+router.put('/updatenote/:id',fetchuser,async(req,res)=>{
+   const {title,description,tag}=req.body;
+   const newNote={};
+   if(title){newNote.title=title};
+   if(title){newNote.description=description};
+   if(title){newNote.tag=tag};
+
+
+   //Find the note to be updated and update it
+   let note=await Notes.findById(req.params.id);
+   if(!note){res.status(404).send("Not Found")}
+ if (note.user.toString()!==req.user.id){
+     return res.status(401).send("Not Allowed");
+ }
+     note =await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+     res.json(note);
+
+    })
+ 
+//Router4:-- Delete a new Note using : Delete"/api/auth/updatenote". Login required
+router.delete('/deletenote/:id',fetchuser,async(req,res)=>{
+    try{
+ const {title,description,tag}=req.body;
+    let note=await Notes.findById(req.params.id);
+    if(!note){res.status(404).send("Not Found")}
+  if (note.user.toString()!==req.user.id){
+      return res.status(401).send("Not Allowed");
+  }
+  note=await Notes.findByIdAndDelete(req.params.id);
+  res.json("Success:The note has been deleted");
+}catch(error){
+    console.log("error.message");
+        res.status(500).send("Internal Server Error Occured");
+}
+})
+
 module.exports=router
